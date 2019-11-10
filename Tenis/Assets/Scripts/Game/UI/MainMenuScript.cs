83efa22ac91b90352cf0581adc9a1fa90cc171f4
Juan Li Puma <juanlipuma94@gmail.com>
2019-11-09 22:18:43 -0300

    First attempt at UI
    - Change starting scene to Main Menu
    - Add two working buttons, Play and Quit

---
+++ Tenis/Assets/Scripts/Game/UI/MainMenuScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void OnPlayClick()
+    {
+        SceneManager.LoadScene("TenisMatch");
+    }
+
+    public void OnQuitClick()
+    {
+        Debug.Log("Quitting game");
+        Application.Quit();
+    }
+}
