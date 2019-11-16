546d35e0021d79f624a7c23b9c952f17220a686e
Agustin Izaguirre <Agustinizag1@gmail.com>
2019-11-16 18:45:25 -0300

    two player added

--- Tenis/Assets/Scripts/Game/Player/Player2Logic.cs
+++ Tenis/Assets/Scripts/Game/Player/Player2Logic.cs
@@ -1,62 +1,165 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using FrameLord;
 using Game.Input;
+using Game.Score;
+using TMPro;
 using UnityEngine;
 
 public class Player2Logic : MonoBehaviour
 {
     // Button to move left
     public KeyCode leftButton = KeyCode.LeftArrow;
+
     // Button to move right
     public KeyCode rightButton = KeyCode.RightArrow;
+
     // Button to move forward
     public KeyCode forwardButton = KeyCode.UpArrow;
+
     // Button to move backward
     public KeyCode bacwardButton = KeyCode.DownArrow;
+
     // Button to hit
     public KeyCode hitButton = KeyCode.A;
 
     public Transform aimTarget;
-    public float aimTargetSpeed;
-    
+    public float aimTargetSpeed = 18;
+
     public float movementSpeed = 14f;
-    public float hitForce = 40f;
+    public float maxHitForce = 40f;
+    private float _serveForce = 40f;
+    private float _currentHitForce;
+    private float _playerSpeed = 50f;
+    private float _maxReach;
+    private float deltaHitForce = 1;
+    public float minHitForce = 18f;
+    
+
+    public float _minimumOffSetToHitBall = -3.0f;
+
+    private ScoreManager _scoreManager;
+    private Side _ballSide;
+    private bool _isServing;
     
     // Is true after hit button is pressed
-    private bool _isHitting;
+    private bool _isCharging;
+
     // Is true after hit button is released;
     private bool _finishHitting;
 
     // Moving Left or Right (-1: left, 1: right, 0: none)
     private int moveLeftRightValue;
+
     // Moving Up or Down (-1: down, 1: up, 0: none)
     private int moveForwardBackwardValue;
+    
     private CharacterController _characterController;
+
+    // Start position of aim target
+    private Vector3 _aimStartPosition;
+
+    /* player id according to court side,
+     * 1 if player is on team one or
+     * 2 if player is on team two
+    */
+    private int _id;
+
+    private GameObject _ball;
+
+    private PlayerAnimation _playerAnimation;
 
     void Start()
     {
         _characterController = GetComponent<CharacterController>();
-        _isHitting = false;
+        _isCharging = false;
         _finishHitting = false;
         moveLeftRightValue = 0;
         moveForwardBackwardValue = 0;
+        _ballSide = Side.RIGHT;
+        _currentHitForce = minHitForce;
+        _playerAnimation =  new PlayerAnimation(GetComponent<Animator>());
+        _aimStartPosition = aimTarget.position;
+        _maxReach = 5f;//TODO update later with sphere collider radius
+        _scoreManager = ScoreManager.GetInstance();
+        SetID();
+        SetIsServing();
+        SetInitialPosition();
+        if (_isServing)
+        {
+            ScoreManager.GetInstance().ActivateServingWalls(_id);
+        }
+        BallLogic.Instance.DesapearBall();
+
+    }
+
+    public void SetInitialPosition()
+    {
+        Vector3 currentPosition = transform.position;
+        _isCharging = false;
+        float x, z; 
+        Side servingSide = _scoreManager.GetServingSide();
+        if (servingSide == Side.RIGHT)
+        {
+            z = 6.57f;
+        }
+        else
+        {
+            z = -6.24f;
+        }
+
+        if (_isServing)
+        {
+            ScoreManager.GetInstance().ActivateServingWalls(_id);
+            x = 32f;
+        }
+        else
+        {
+            x = 26.0f;
+        }
+
+        _characterController.enabled = false;
+        Vector3 newPosition = new Vector3(x, currentPosition.y, z);
+        transform.position = newPosition;
+        _characterController.enabled = true;
+    }
+
+    private void SetIsServing()
+    {
+        _isServing = _id == _scoreManager.GetServingTeam();
+    }
+
+    private void SetID()
+    {
+        if (transform.position.x < 0)
+        {
+            _id = 1;
+        }
+        else
+        {
+            _id = 2;
+        }
+
     }
 
     void Update()
     {
-        ReadInput();
+        if (!_playerAnimation.IsPlayingHitAnimation())
+        {
+            ReadInput();
 
-        if(!_isHitting)
-        {
-            UpdatePosition();
-        }
-        else
-        {
-            UpdateAimTargetPosition();
+            if (!_isCharging)
+            {
+                UpdatePosition();
+            }
+            else
+            {
+                UpdateAimTargetPosition();
+            }
         }
     }
-    
+
     private void ReadInput()
     {
         moveLeftRightValue = 0;
@@ -67,7 +170,7 @@ public class Player2Logic : MonoBehaviour
         {
             moveLeftRightValue += -1;
         }
-        
+
         if (ActionMapper.GetMoveRight(rightButton))
         {
             moveLeftRightValue += 1;
@@ -77,7 +180,7 @@ public class Player2Logic : MonoBehaviour
         {
             moveForwardBackwardValue += 1;
         }
-        
+
         if (ActionMapper.GetMoveBackward(bacwardButton))
         {
             moveForwardBackwardValue += -1;
@@ -85,37 +188,170 @@ public class Player2Logic : MonoBehaviour
 
         if (ActionMapper.GetHitPressed(hitButton))
         {
-            _isHitting = true;
+            if (!_isCharging)
+            {
+                _currentHitForce = minHitForce;
+                aimTarget.position = _aimStartPosition;
+                if (!_isServing)
+                {
+                    _ballSide = BallLogic.Instance.GetSide(transform.position);
+                    _playerAnimation.StartHittingAnimation(_ballSide);
+                }
+
+            }
+
+            _isCharging = true;
+            _currentHitForce += _currentHitForce + deltaHitForce;
+            _currentHitForce = Math.Min(_currentHitForce, maxHitForce);
         }
 
         if (ActionMapper.GetHitReleased(hitButton))
         {
-            _isHitting = false;
-            _finishHitting = true;
+//            _isCharging = false;
+//            _finishHitting = true;
+            if (_isServing)
+            {
+                _isCharging = false;
+                _playerAnimation.StartServeAnimation();
+            }
+//            else
+//            {
+//                //DetectBallSide();
+//                _playerAnimation.StartHittingAnimation(_ballSide);
+//            }
         }
     }
-    
+
     private void UpdatePosition()
     {
         float leftRightMove = movementSpeed * moveLeftRightValue * Time.deltaTime;
         float forwardBackardMove = movementSpeed * moveForwardBackwardValue * Time.deltaTime;
-
-        _characterController.Move(new Vector3(leftRightMove, 0, forwardBackardMove));
+        _playerAnimation.AnimateMovement(leftRightMove, forwardBackardMove);
+        var vec = new Vector3(forwardBackardMove, 0, -leftRightMove);
+        _characterController.SimpleMove(vec * _playerSpeed);
+        //TODO steps sound as animation event
+       // AudioManager.Instance.PlaySound(transform.position, (int) SoundId.SOUND_STEPS); 
     }
+
+   
 
     private void UpdateAimTargetPosition()
     {
-        aimTarget.Translate(new Vector3(aimTargetSpeed * moveLeftRightValue * Time.deltaTime, 0, aimTargetSpeed * moveForwardBackwardValue * Time.deltaTime));
+        aimTarget.Translate(new Vector3(aimTargetSpeed * moveForwardBackwardValue * Time.deltaTime, 0,
+            -aimTargetSpeed * moveLeftRightValue * Time.deltaTime));
+    }
+
+//    private void OnTriggerStay(Collider other)
+//    {
+//        if (other.CompareTag("Ball"))
+//        {
+//            DetectBallSide(other);
+//            
+//            if (_finishHitting)
+//            {
+//                _ball = other.gameObject;
+//            }
+//        }
+//    }
+
+//    private void DetectBallSide(Collider other)
+//    {
+//        Vector3 deltaPosition = other.gameObject.transform.position - transform.position;
+//        // Positive = left
+//        // Negative = right
+//        Debug.Log(deltaPosition.z <= 0 ? "RIGHT" : "LEFT");
+//        _ballSide = deltaPosition.z <= 0 ? Side.RIGHT : Side.LEFT;
+//    }
+    
+//    private void DetectBallSide()
+//    {
+//        Vector3 bsllPosition = BallLogic.Instance.transform.position;
+//        Vector3 deltaPosition = bsllPosition - transform.position;
+//        // Positive = left
+//        // Negative = right
+//        _ballSide = deltaPosition.z <= 0 ? Side.RIGHT : Side.LEFT;
+//    }
+
+    private bool IsValidBallPositionToHit(GameObject ballToHit)
+    {
+        return (ballToHit.transform.position - transform.position).x >= _minimumOffSetToHitBall;
+    }
+
+//    private void HitBall()
+//    {
+//        if (_ball != null && IsValidBallPositionToHit(_ball))
+//        {
+//            AudioManager.Instance.PlaySound(_ball.transform.position, (int) SoundId.SOUND_HIT);
+//            Vector3 aimDirection = (aimTarget.position - transform.position).normalized;
+//            _ball.GetComponent<Rigidbody>().velocity = aimDirection * _currentHitForce + new Vector3(0, 6.2f, 0);
+//            _currentHitForce = minHitForce;
+//            BallLogic.Instance.SetHittingPlayer(_id);
+//        }
+//    }
+    private void HitBall()
+    {
+        if (_ball != null)
+        {
+            AudioManager.Instance.PlaySound(_ball.transform.position, (int) SoundId.SOUND_HIT);
+            Vector3 aimDirection = (aimTarget.position - transform.position).normalized;
+            Vector3 velocity = BallLogic.Instance.GetVelocity(aimTarget.position, 1.5f);//change time in function of currentHitForce
+//            _ball.GetComponent<Rigidbody>().velocity = aimDirection * _currentHitForce + new Vector3(0, 6.2f, 0);
+            _ball.GetComponent<Rigidbody>().velocity = velocity;
+            _currentHitForce = minHitForce;
+            BallLogic.Instance.SetHittingPlayer(_id);
+        }
+
+        _finishHitting = true;
+        _isCharging = false;
+    }
+
+    private void Serve()
+    {
+        Vector3 currentPosition = transform.position;
+        Vector3 aimDirection = (aimTarget.position - currentPosition).normalized;
+        //float serveForce = 40f; //TODO use a private variable for serve force
+        BallLogic ball = BallLogic.Instance;
+        ball.AppearBall(new Vector3(currentPosition.x + 0.1f, 4.05f, currentPosition.z), Vector3.zero );
+        ball.GetComponent<Rigidbody>().velocity = aimDirection * _serveForce + new Vector3(0, -1.2f, 0);//TODO update with physics
+        BallLogic.Instance.SetHittingPlayer(_id);
     }
     
+//    private void DeleteBallReference()
+//    {
+//        _ball = null;
+//    }
+
 
     private void OnTriggerEnter(Collider other)
     {
+        
         if (other.CompareTag("Ball"))
         {
-            Vector3 aimDirection = (aimTarget.position - transform.position).normalized;
-            
-            other.GetComponent<Rigidbody>().velocity = aimDirection * hitForce + new Vector3(0, 6.2f, 0);
+            _ball = null;
+            _playerAnimation.EndHittingAnimation();
+            float distance = BallLogic.Instance.GetDistance(transform.position);
+
+            if (distance <= _maxReach && _isCharging && BallLogic.Instance.GetHeight() < 3.85f)
+            {
+                _ball = other.gameObject;
+                //todo animation of hit activate value
+                HitBall();
+            }
         }
+    }
+    
+    public void SetServing(bool serving)
+    {
+        _isServing = serving;
+    }
+
+    public int GetId()
+    {
+        return _id;
+    }
+
+    private void PlayServeSound()
+    {
+        AudioManager.Instance.PlaySound(transform.position, (int) SoundId.SOUND_SERVE);
     }
 }
