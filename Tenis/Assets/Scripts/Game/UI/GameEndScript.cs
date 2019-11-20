f959428f63513b0cb7f64bc6ef957231f8d51bd7
Agustin Izaguirre <Agustinizag1@gmail.com>
2019-11-20 12:48:01 -0300

    text changed when 2 players

--- Tenis/Assets/Scripts/Game/UI/GameEndScript.cs
+++ Tenis/Assets/Scripts/Game/UI/GameEndScript.cs
@@ -12,7 +12,15 @@ namespace Game.UI
         public void Start()
         {
             bool playerWon = GameManagerLogic.GetPlayerWon();
-            endGameText.text = playerWon ? "You Win" : "You Lose";
+            if (ScoreManager.GetInstance().IsTwoPlayers())
+            {
+                endGameText.text = playerWon ? "Player 1 Wins!" : "Player 2 Wins!";
+
+            }
+            else
+            {
+                endGameText.text = playerWon ? "You Win!" : "You Lose";
+            }
         }
 
         public void OnMainMenuClick()
